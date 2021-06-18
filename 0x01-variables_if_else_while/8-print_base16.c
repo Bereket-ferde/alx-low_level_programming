63e35481ccedbd57bfbcd4338e0074ef8d964bbe
Bereket-ferde <bereketferde22375378@gmail.com>
2021-06-18 06:04:46 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - main block
  * main - Entry point
+ * Description: prints all single digit numbers of base 16
+ * starting from 0, followed by a new line.
  * Return:0
  */
 int main(void)
 {
 	int i, y;
+	
 	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
