f8b523ca773b27bc493056627bd0bb0c7a6d2128
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-06-28 18:41:15 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,24 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/**
-*
-*
-*/
-int main(void)
-{
+#include <time.h>
 
-char num = rand();
-srand(time(0));
+int main()
+{   
+    int i, j; 
+    int RandomNumber; 
+    char letter[0]; 
+    char word[8];
+    char testword[4] = "test";
 
-printf("%i\n", num);
-return (0);
+    // generate random word 
+
+    for (j = 3; j < 10; j++) {
+        RandomNumber = rand() % 26;
+        printf("%d", RandomNumber); 
+        word[j] = 'a' + RandomNumber; 
+        printf("Word: %s", word[j]); 
+    }
+
+    return 0;
 }
