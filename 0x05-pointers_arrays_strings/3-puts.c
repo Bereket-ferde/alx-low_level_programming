df0d5146a84d91feb322f1b3a79a97885236d72d
Bereket-ferde <bereketferde22375378@gmail.com>
2021-06-28 06:28:30 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string followed by new line
+ * @str: string str
+ */
+void _puts(char *str)
+{
+	while (*s)
+	{
+	        _putchar(*str);
+		str += 1;
+	}
+	_putchar('\n');
+}
