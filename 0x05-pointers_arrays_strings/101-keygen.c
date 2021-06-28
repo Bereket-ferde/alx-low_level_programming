2ed59c6a8c4e83f9aa2546ce6c482a816c8ad771
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-06-28 18:45:09 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,19 +5,16 @@
 
 int main()
 {   
-    int i, j; 
+    int j; 
     int RandomNumber; 
-    char letter[0]; 
     char word[8];
-    char testword[4] = "test";
 
-    // generate random word 
+    /* generate random word */ 
 
     for (j = 3; j < 10; j++) {
         RandomNumber = rand() % 26;
         printf("%d", RandomNumber); 
         word[j] = 'a' + RandomNumber; 
-        printf("Word: %s", word[j]); 
     }
 
     return 0;
