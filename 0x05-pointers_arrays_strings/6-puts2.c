7e68f2b7a0b94132c387a38de60a76eafc79d94b
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-06-28 17:20:13 +0300

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,34 +1,21 @@
 #include "holberton.h"
-
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
 
-	int length = 0;
-
-	while (*s)
-	{
-		s++;
-		length++;
-	}
-	return (length);
-}
-
-/**
- * puts2 - prints one char out of 2 of a string, followed by
- * a new line
- * @str: string str
- */
 void puts2(char *str)
 {
-	int i;
-	int size = _strlen(str);
+int i = 0;
+while (str[i] != '\0')
+{
+	if (i % 2 == 0)
+	{
+		_putchar(str[i]);
+	}
 
-	for (i = 0; i < size; i += 2)
-		_putchar(*(str + i));
-	_putchar('\n');
+	i++;
+}
+_putchar('\n');
 }
