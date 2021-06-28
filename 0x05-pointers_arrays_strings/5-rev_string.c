82fee10474a55f3bd1a399ab67e79aab224b7c2b
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-06-28 17:13:17 +0300

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
+* _strlen - returns the length of a string
+* @s: string s
+* Return: length of string
+*/
 int _strlen(char *s)
 {
 	int length = 0;
