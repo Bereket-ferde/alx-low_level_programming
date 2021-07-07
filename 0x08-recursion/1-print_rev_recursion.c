b1c7eccef4d3eb91f6edb38932254fc565fbdff7
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-07-07 18:37:54 +0300

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,9 +4,9 @@
  * _print_rev_recursion - prints a string in reverse, followed by a new line.
  * @s: string to be printed
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
