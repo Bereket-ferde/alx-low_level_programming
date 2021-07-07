f2efe0864a0da8342bb88b414f9e82cd59aceee9
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-07-07 18:25:13 +0300

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - print the given character
- * *s - points the value of the given character
+ * _puts_recursion - print the given character.
+ * @s: points the value of the given character
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                _putchar('\n');
-                return;
-        }
-        _putchar(*s);
-        _puts_recursion(++s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
