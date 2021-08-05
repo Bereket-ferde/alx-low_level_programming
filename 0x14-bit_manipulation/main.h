189aeb86dc2cc72b466d2aededf04648921d29f0
Bereket-ferde <84667103+Bereket-ferde@users.noreply.github.com>
2021-08-05 23:23:19 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,4 +18,4 @@ int get_endianness(void);
 int _strlen(const char *s);
 _Bool isBinary(char);
 
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
